693e6421920ea9ec8a98626a2f7133cc589e9326
jaumeferDAM <jaumefernandezacanmas@gmail.com>
2017-01-11 19:20:15 +0100

    Iniciar los editText/Login

--- app/src/main/java/com/example/marcosportatil/geolocalitzacio/MainActivity.java
+++ app/src/main/java/com/example/marcosportatil/geolocalitzacio/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.marcosportatil.geolocalitzacio;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EditText matriculaEditText = (EditText) findViewById(R.id.editTextMatricula);
+        EditText passwordEditText = (EditText) findViewById(R.id.editTextPassword);
+
+        Button loginButton = (Button) findViewById(R.id.buttonLogin);
+        loginButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
