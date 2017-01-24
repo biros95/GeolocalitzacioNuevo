9162e70ed483a4a95d6bf7def0f7cf52595dc0df
Marcos <biros1995@gmail.com>
2017-01-24 16:30:54 +0100

    modificacion dbhelper

--- app/src/main/java/com/example/marcosportatil/geolocalitzacio/MainActivity.java
+++ app/src/main/java/com/example/marcosportatil/geolocalitzacio/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.marcosportatil.geolocalitzacio;
 
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,12 +9,19 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private SQLiteDatabase db;
+    private String login;
+    private String password;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         EditText matriculaEditText = (EditText) findViewById(R.id.editTextMatricula);
         EditText passwordEditText = (EditText) findViewById(R.id.editTextPassword);
+        login = matriculaEditText.getText().toString();
+        password = passwordEditText.getText().toString();
+
+
 
         Button loginButton = (Button) findViewById(R.id.buttonLogin);
         loginButton.setOnClickListener(this);
